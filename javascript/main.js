76b06126c23525a1ed1ca7a562c3634d9bd48743
The_Manux <66367222+Manux895@users.noreply.github.com>
2023-02-05 23:21:50 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,27 @@
 var lastScrollTop = 0;
 let timer = null;
 const navbar = document.getElementById("menu");
+
+// questa parte serve a non nascondere la navbar se il cursore è sopra di essa
+//? cancellare questa parte (lasciando solo la riga 17) per tornare alla vecchia navbar
+navbar.addEventListener("mouseover", function() {
+    clearTimeout(timer);
+});
+navbar.addEventListener("mouseout", function() {
+    // imposta un nuovo timer per nascondere la navbar dopo un secondo e mezzo che non ci si trova su di essa
+    timer = setTimeout(function() {
+        navbar.style.top = "-100px";
+    }, 1.5 * 1000);
+});
+// questa parte nasconde la navbar se non si scolla per un secondo e mezzo e non si è all'inizio della home
 window.addEventListener("scroll", function(){
     clearTimeout(timer);
     navbar.style.top = "0px"
-    timer = setTimeout(function(){ // nascondi la barra se non si scolla per un secondo e mezzo e non si è a inizio home
+    timer = setTimeout(function(){
         if (window.pageYOffset != 0) {
             navbar.style.top = "-100px";
         }
-    }, 1.5 * 1000);
+    }, 1.5 * 1000); 
 
     //? nascondi la barra se si scorre in basso e mostrala se si scorre in alto. Rinascondila se non si scorre per 5 secondi
     // clearTimeout(timer);
@@ -25,7 +38,7 @@ window.addEventListener("scroll", function(){
     //     }
     // }, 5 * 1000);
 
-    // la parte di funzione che segue serve ad attivare e disattivare i link nella navbar durabte lo scroll
+    // questa parte serve ad attivare e disattivare i link nella navbar durante lo scroll
     const divs = document.querySelectorAll("#home_Page, #educazione_civica_Page, #materie_Page");
     const navLinks = document.querySelectorAll("#home, #educazione_civica, #materie");
     for (let i = 0; i < 3; i++) {
@@ -42,15 +55,53 @@ window.addEventListener("scroll", function(){
     }
 })
 
-function goTo(daAttivare) { // attiva il link cliccato della navbar e scrolla fino al div corrispondente
+function goTo(daAttivare) { // attiva il link cliccato nella navbar e scrolla fino al div corrispondente
     const link = document.querySelector("nav a.active");
-    link.classList.remove("active");
-    document.getElementById(daAttivare).classList.add("active");
+    link.classList.remove("active", "hover"); // la classe hover serve a prolungare l'effetto hover fino a fine animazione
+    document.getElementById(daAttivare).classList.add("active", "hover");
     
     const targetDiv = document.getElementById(daAttivare + "_Page");
     window.scrollTo({
         top: targetDiv.offsetTop,
     });
 }
-//TODO quando scorre verso l'alto la navbar resta ferma e quando scorre verso il basso sparisce. Deve restare ferma sempre
-//TODO su mobile la navbar fa casino con gli hover
+//TODO se scorri durante l'animazione scrollTo la classe hover non viene rimossa
+//TODO quando il cursore passa all'altezza della scrollbar devi rimostrarla
+//! (con vecchia navbar) quando scorre verso l'alto la navbar resta ferma e quando scorre verso il basso sparisce. Deve restare ferma sempre
+//TODO su mobile la navbar fa casino con gli hover se tieni premuto ed evidenzia link del menu quando li premi
+
+
+
+// var scrolling = false;
+// var stopScrolling = false;
+
+// function goTo(daAttivare) {
+//     const link = document.querySelector("nav a.active");
+//     link.classList.remove("active", "hover");
+//     document.getElementById(daAttivare).classList.add("active", "hover");
+
+//     const targetDiv = document.getElementById(daAttivare + "_Page");
+
+//     scrolling = true;
+//     window.scrollTo({
+//         top: targetDiv.offsetTop,
+//     });
+// }
+
+// window.addEventListener("scroll", function(){
+//     if (scrolling && stopScrolling) {
+//         // interrompere l'animazione di scorrimento
+//         scrolling = false;
+//         stopScrolling = false;
+//         window.scrollTo(0, 0);
+//         console.log("kad");
+//     const link = document.querySelector("nav a.active");
+//     link.classList.remove("hover");
+//     }
+// });
+
+// // questa funzione interrompe l'animazione di scorrimento
+// function stopGoTo() {
+//     stopScrolling = true;
+//     console.log("kad");
+// }
