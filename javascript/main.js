82c0f12506304af08422f25ed9bc28df56044b0b
The_Manux <66367222+Manux895@users.noreply.github.com>
2023-02-06 21:31:03 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,35 @@
 var lastScrollTop = 0;
 let timer = null;
+let mouseOverNavbar = false;
 const navbar = document.getElementById("menu");
 
 // questa parte serve a non nascondere la navbar se il cursore è sopra di essa
-//? cancellare questa parte (lasciando solo la riga 19) per tornare alla vecchia navbar
+//? cancellare questa parte (lasciando solo la riga 22) per tornare alla vecchia navbar
 navbar.addEventListener("mouseover", function() {
+    mouseOverNavbar = true;
     clearTimeout(timer);
-    console.log("InizioOver");
 });
 navbar.addEventListener("mouseout", function() {
     // imposta un nuovo timer per nascondere la navbar dopo un secondo e mezzo che non ci si trova su di essa
+    mouseOverNavbar = false;
     timer = setTimeout(function() {
         if (window.pageYOffset != 0) {
             navbar.style.top = "-100px";
-            console.log("FineOver");
         }
     }, 1.5 * 1000);
 });
+
 // questa parte nasconde la navbar se non si scolla per un secondo e mezzo e non si è all'inizio della home
 window.addEventListener("scroll", function(){
     clearTimeout(timer);
     navbar.style.top = "0px";
-    console.log("InizioScroll");
-
-    timer = setTimeout(function(){
-        if (window.pageYOffset != 0) {
-            navbar.style.top = "-100px";
-            console.log("FineScroll");
-        }
-    }, 1.5 * 1000); 
+    if (!mouseOverNavbar) { // prima di nascondere la navbar dopo lo scroll controlla che il mouse non si trovi sopra di essa (evita bug non movimento mouse quando appare la navbar)
+        timer = setTimeout(function(){
+            if (window.pageYOffset != 0) {
+                navbar.style.top = "-100px";
+            }
+        }, 1.5 * 1000);
+    }
 
     //? nascondi la barra se si scorre in basso e mostrala se si scorre in alto. Rinascondila se non si scorre per 5 secondi
     // clearTimeout(timer);
@@ -45,7 +46,7 @@ window.addEventListener("scroll", function(){
     //     }
     // }, 5 * 1000);
 
-    // questa parte serve ad attivare e disattivare i link nella navbar durante lo scroll
+    // questa parte serve ad attivare e disattivare i link nella navbar durante lo scroll (sia scroll automatico sia manuale)
     const divs = document.querySelectorAll("#home_Page, #educazione_civica_Page, #materie_Page");
     const navLinks = document.querySelectorAll("#home, #educazione_civica, #materie");
     for (let i = 0; i < 3; i++) {
@@ -107,8 +108,7 @@ function goTo(daAttivare) {
 
 
 
-//TODO quando si scorre e si ha già il cursore sulla navbar questa non resta visibile
-//! classe hover
+//! classe hover da attivare o disattivare
 //? quando il cursore passa all'altezza della scrollbar devi rimostrarla
 //! (con vecchia navbar) quando scorre verso l'alto la navbar resta ferma e quando scorre verso il basso sparisce. Deve restare ferma sempre
 //TODO su mobile la navbar fa casino con gli hover se tieni premuto ed evidenzia link del menu quando li premi
