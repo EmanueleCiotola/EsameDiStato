0019ed8a37204a79546d1a15862114a59a25a8b0
The_Manux <66367222+Manux895@users.noreply.github.com>
2023-02-06 17:45:13 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -3,15 +3,17 @@ let timer = null;
 const navbar = document.getElementById("menu");
 
 // questa parte serve a non nascondere la navbar se il cursore è sopra di essa
-//? cancellare questa parte (lasciando solo la riga 17) per tornare alla vecchia navbar
+//? cancellare questa parte (lasciando solo la riga 19) per tornare alla vecchia navbar
 navbar.addEventListener("mouseover", function() {
     clearTimeout(timer);
+    console.log("InizioOver");
 });
 navbar.addEventListener("mouseout", function() {
     // imposta un nuovo timer per nascondere la navbar dopo un secondo e mezzo che non ci si trova su di essa
     timer = setTimeout(function() {
         if (window.pageYOffset != 0) {
             navbar.style.top = "-100px";
+            console.log("FineOver");
         }
     }, 1.5 * 1000);
 });
@@ -19,10 +21,12 @@ navbar.addEventListener("mouseout", function() {
 window.addEventListener("scroll", function(){
     clearTimeout(timer);
     navbar.style.top = "0px";
+    console.log("InizioScroll");
 
     timer = setTimeout(function(){
         if (window.pageYOffset != 0) {
             navbar.style.top = "-100px";
+            console.log("FineScroll");
         }
     }, 1.5 * 1000); 
 
@@ -56,59 +60,55 @@ window.addEventListener("scroll", function(){
             navLinks[i].classList.remove("active");
         }
     }
-})
+});
 
-function goTo(daAttivare) {
-    const link = document.querySelector("nav a.active");
-    link.classList.remove("active");
-
+function goTo(daAttivare) {    
     const targetDiv = document.getElementById(daAttivare + "_Page");
     window.scroll({
         top: targetDiv.offsetTop,
     });
-
-    document.getElementById(daAttivare).classList.add("active");
 }
+
+// function goTo(daAttivare) {
+//     document.getElementById(daAttivare).classList.add("hover");
+    
+//     // questa parte aggiunge rimuove hover se la transizione termina normalmente
+//     const targetDiv = document.getElementById(daAttivare + "_Page");
+//     let animationEnded = false;
+//     targetDiv.addEventListener("transitionend", function() {
+//         animationEnded = true;
+//         document.getElementById(daAttivare).classList.remove("hover");
+//         targetDiv.removeEventListener("transitionend", this);
+//     });
+    
+//     // questa parte rimuove hover se la transizione viene interrotta con scroll manuale
+//     new Promise(resolve => {
+//         let previousY = window.pageYOffset;
+//         const handleScroll = setInterval(() => {
+//             const currentY = window.pageYOffset;
+//             if (previousY === currentY) { // se la posizione Y non è cambiata, significa che lo scorrimento è terminato
+//                 resolve();
+//                 clearInterval(handleScroll);
+//             }
+//             previousY = currentY;
+//         }, 50);
+//     }).then(() => {
+//             animationEnded = true;
+//             document.getElementById(daAttivare).classList.remove("hover");
+//         })
+//         .catch(() => {
+//             // La Promise viene interrotta se c'è un errore o se viene chiamato il metodo `reject`
+//             animationEnded = true;
+//             document.getElementById(daAttivare).classList.remove("hover");
+//         });
+    
+//     window.scrollTo({top: targetDiv.offsetTop});
+// }
 
 
 
 //TODO quando si scorre e si ha già il cursore sulla navbar questa non resta visibile
+//! classe hover
 //? quando il cursore passa all'altezza della scrollbar devi rimostrarla
 //! (con vecchia navbar) quando scorre verso l'alto la navbar resta ferma e quando scorre verso il basso sparisce. Deve restare ferma sempre
 //TODO su mobile la navbar fa casino con gli hover se tieni premuto ed evidenzia link del menu quando li premi
-
-
-
-// var scrolling = false;
-// var stopScrolling = false;
-
-// function goTo(daAttivare) {
-//     const link = document.querySelector("nav a.active");
-//     link.classList.remove("active", "hover");
-//     document.getElementById(daAttivare).classList.add("active", "hover");
-
-//     const targetDiv = document.getElementById(daAttivare + "_Page");
-
-//     scrolling = true;
-//     window.scrollTo({
-//         top: targetDiv.offsetTop,
-//     });
-// }
-
-// window.addEventListener("scroll", function(){
-//     if (scrolling && stopScrolling) {
-//         // interrompere l'animazione di scorrimento
-//         scrolling = false;
-//         stopScrolling = false;
-//         window.scrollTo(0, 0);
-//         console.log("kad");
-//     const link = document.querySelector("nav a.active");
-//     link.classList.remove("hover");
-//     }
-// });
-
-// // questa funzione interrompe l'animazione di scorrimento
-// function stopGoTo() {
-//     stopScrolling = true;
-//     console.log("kad");
-// }
