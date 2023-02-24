73e348e264b3b0747553f2cafe9bb8cbe9d7c4b1
The_Manux <66367222+Manux895@users.noreply.github.com>
2023-02-24 17:48:29 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -2,17 +2,19 @@
 var elementiDaOsservare = document.querySelectorAll('.osservato');
 var callback = function(items) {
     items.forEach((item) => {
+        var position = item.target.getBoundingClientRect();
         if (item.isIntersecting) {
             item.target.classList.add("mostrato");
-        } else {
+        } else if (position.top > 0) { // rimuove la classe mostrato solo se l'item si trova nella parte non visibile superiore dello schermo
             item.target.classList.remove("mostrato");
         }
     });
 }
-var observer = new IntersectionObserver(callback, {threshold: 0.8});
+var observer = new IntersectionObserver(callback, {threshold: 0.6});
 elementiDaOsservare.forEach((element) => {
     observer.observe(element);
 });
+
 
 
 
