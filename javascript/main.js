e9488731aa9c1824adb160850b29019a99ba204b
The_Manux <66367222+Manux895@users.noreply.github.com>
2023-06-07 17:27:07 +0200

    Codice pulito

--- javascript/main.js
+++ javascript/main.js
@@ -17,8 +17,4 @@ elementiDaOsservare.forEach((element) => {
 
 
 
-
-//! classe hover da attivare o disattivare
-//? quando il cursore passa all'altezza della scrollbar devi rimostrarla
-//! (con vecchia navbar) quando scorre verso l'alto la navbar resta ferma e quando scorre verso il basso sparisce. Deve restare ferma sempre
 //TODO su mobile la navbar fa casino con gli hover se tieni premuto ed evidenzia link del menu quando li premi
